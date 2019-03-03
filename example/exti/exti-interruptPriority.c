97bd4e185327a099b751945605ba8d148f6233bd
zhongliguo <1695130178@qq.com>
2019-03-03 15:07:35 +0800

    style: adjust coding style example/exti/exti-interruptPriority.c

--- example/exti/exti-interruptPriority.c
+++ example/exti/exti-interruptPriority.c
@@ -4,10 +4,10 @@
  * @Compiler:SDCC v3.6.0
  * @E-mail:mcu(at)zhishan-iot.tk
  * @File-description:a example which shows how to use HML to control interruptPriority between EXTI and TIM
- * @Support-mcu:Intel MCS-52 based microprocessors
+ * @Support-mcu:STC micro STC89 series
  * @Test-board:ZS51-V1.0
  * @Test-mcu:STC89C52RC
- * @Version:V0
+ * @Version:V1
  */
  
  /*
@@ -56,12 +56,12 @@ void main(void)
 }
 
 /*
- * @Prototype:void TIM_isr(void)
+ * @Prototype:void tim0_isr(void)
  * @Parameter:None
  * @Ret-val:None
- * @Note:interrupt service function for TIM_0
+ * @Note:interrupt service function for TIM0
  */
-void TIM_isr(void) __interrupt TF0_VECTOR
+void tim0_isr(void) __interrupt TF0_VECTOR
 {
     static int cnt = 0;
     
